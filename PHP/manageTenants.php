df79e0ec7c5dc29075f4e878bc7d50faaab7b95b
Ralph Lucero <ralphluceroxx@gmail.com>
2025-10-26 21:42:23 +0800

    Fixed Logout

--- PHP/manageTenants.php
+++ PHP/manageTenants.php
@@ -87,7 +87,7 @@ $tenants = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         echo htmlspecialchars(strtoupper($initials) ?: 'U');
                     ?>
                 </div>
-                <a href="../logout.php" title="Logout" class="p-2 text-slate-600 hover:text-red-600">
+                <a href="logout.php" title="Logout" class="p-2 text-slate-600 hover:text-red-600">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                               d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
